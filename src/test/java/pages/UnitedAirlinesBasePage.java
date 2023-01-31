4cd31acecfc1636f630e72e1c4c8b4e6416bd2dd
Newer Zeidan <newerzeidan@yahoo.com>
2023-01-31 14:19:09 -0600

    project 03

--- src/test/java/pages/UnitedAirlinesBasePage.java
+++ src/test/java/pages/UnitedAirlinesBasePage.java
@@ -30,4 +30,43 @@ public class UnitedAirlinesBasePage {
 
     @FindBy(id = "oneway")
     public List<WebElement> oneWayButton;
+
+    @FindBy(css = "input[name='AwardTravel']")
+    public List<WebElement> bookWithMilesButton;
+
+    @FindBy(css = "label[for='award']")
+    public List<WebElement> bookWithMilesLabel;
+
+    @FindBy(id = "flexibleDates")
+    public List<WebElement> flexibleDatesButton;
+
+    @FindBy(css = "label[for*='flexibleDates']")
+    public List<WebElement> flexibleDatesLabel;
+
+    @FindBy(id = "bookFlightOriginInput")
+    public WebElement fromInputBox;
+
+    @FindBy(id = "bookFlightDestinationInput")
+    public WebElement toInputBox;
+
+    @FindBy(id = "DepartDate")
+    public WebElement datesBox;
+
+    @FindBy(xpath = "//button[@aria-labelledby='stuff']")
+    public WebElement travelerButton;
+
+    @FindBy(xpath = "//button[@aria-label='Substract one Adult']")
+    public WebElement travelerSelector;
+
+    @FindBy(id = "cabinType")
+    public WebElement cabinType;
+
+    @FindBy(css = "li[id*='cabinType_item']")
+    public List<WebElement> cabinDropdownOptions;
+
+    @FindBy(xpath = "//button[@aria-label='Find flights']")
+    public WebElement findFlightsButton;
+
+    @FindBy(xpath = "//div[@class='app-components-Shopping-ColumnSortResultHeader-styles__headerLeft--3_5-b']")
+    public WebElement result;
 }
